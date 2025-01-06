d6adf992b7d2e4eb5c7e4d5343aa0b9977f8e4ab
jmitchell35 <julian.m.mitchell@gmail.com>
2025-01-06 15:38:50 +0100

    Updating simple_shell.c to handle EOF, both interactive and non-interactive modes

--- simple_shell.c
+++ simple_shell.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
 	size_t len = 0; /* taille initiale buffer alloué */
 	char *token; /* stockage pointeur mot extrait par strtok */
 	char *delim = " \n"; /* délimitateur de mot */
-	int i = 0; /* index écrire dans le tableau */
+	int i; /* index écrire dans le tableau */
 	char *array[1024]; /* stockage mot extrait */
 
 	if (argc != 1) /* vérification validité nb argument */
@@ -24,26 +24,42 @@ int main(int argc, char *argv[])
 	}
 
 	while (1) /* boucle infini prompt */
-	{
-		printf("écris_tes_morts$ "); /* affichage prompt */
-		fflush(stdout); /* forcer écriture immédiate */
+	{		
+		if (isatty(STDIN_FILENO)) /* Vérifie exécution interactive */
+		{
+			printf("#cisfun$ "); /* affichage prompt */
+			fflush(stdout); /* forcer écriture immédiate */
+		}
 
 		if (getline(&line, &len, stdin) == - 1) /* vérification lecture a échoué */
 		/* lire ligne d'entrée à partir de stdin(clavier) */
 		{
-			perror(argv[0]);
-			free(line); /* libération mémoire alloué à line par getline() */
-			return (1); /* indication erreur */
+			if (feof(stdin)) /* Gère le Ctrl + D */
+			{
+				if (isatty(STDIN_FILENO)) /* Si intéractif */
+					printf("\n");
+				break;
+			}
+			else /* Autres erreurs de getline */
+			{
+				perror(argv[0]);
+				free(line); /* libération mémoire alloué à line par getline() */
+				return (1); /* indication erreur */
+			}
 		}
 
+		i = 0;
 		token = strtok(line, delim); /* découpage line en sous chaîne */
 		while (token) /* tant que mots à extraire */
 		{
 			array[i++] = token; /* stockage de chaque mot trouvé */
 			token = strtok(NULL, delim); /* appel pour continuer analyse line */
 		}
-		array[i] = NULL; /* ajout NULL élément -> execve sait 
-								où s'arrète liste d'arguments */
+		array[i] = NULL; 
+		/* Arrête la liste d'arguments i avec le char null */
+
+		if (array[0] == NULL)
+			continue;
 
 		pid = fork(); /* création processus enfant */
 		if (pid == - 1) /* vérification échec processus */
@@ -56,15 +72,16 @@ int main(int argc, char *argv[])
 		{
 			if (execve(array[0], array, NULL) == -1)
 			/* vérification exécution valide */
+			{
 				perror(argv[0]);
 				free(line); /* libération mémoire alloué à line par getline() */
 				exit(EXIT_FAILURE); /* termine processus enfant avec erreur */
+			}
 		}
 		else /* Sinon processus parent */
 		{
 			wait(&status); /* instruction pour ce dernier */
 		}
-		i = 0;
 	}	
 	free(line); /* libération mémoire alloué à line par getline() */
 	return (0);
