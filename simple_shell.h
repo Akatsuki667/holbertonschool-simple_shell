88989c3861d174be9d4ff05d637cd181fabfd9e8
jmitchell35 <julian.m.mitchell@gmail.com>
2025-01-09 15:13:25 +0100

    updating header file

--- simple_shell.h
+++ simple_shell.h
@@ -11,5 +11,8 @@
 void _tokenize(char *str, char *argv);
 void exec_cmd(char *line, char *array[1024], char *argv);
 char *check_for_exe_in_path(char *array, char *argv);
+char *_getenv(const char *name);
+
+extern char **environ;
 
 #endif
